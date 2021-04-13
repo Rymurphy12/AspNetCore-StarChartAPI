3dc61b953bd89d38100a8fef305a5d4f3c669df0
Ryan Murphy <rymurphy12@gmail.com>
2021-04-13 18:11:58 -0400

    Added Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,51 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var model = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+
+            if ( model == null)
+            {
+                return NotFound();
+            }
+
+            model.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == id).ToList();
+
+            return Ok(model);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var results = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+
+            if (results == null || results.Count == 0)
+            {
+                return NotFound();
+            }
+
+            foreach (var result in results)
+            {
+                result.Satellites = _context.CelestialObjects.Where(x => x.Id == result.Id).ToList();
+            }
+            
+            return Ok(results);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var results = _context.CelestialObjects.ToList();
+
+            foreach (var result in results)
+            {
+                result.Satellites = _context.CelestialObjects.Where(x => x.Id == result.Id).ToList();
+            }
+
+            return Ok(results);
+        }
     }
 }
